5f15f8ae192a6fed3dc200ce67ab55ffcbdf6d39
Tamar <mischegoss@gmail.com>
2021-07-25 19:33:52 -0400

    Add repo to url

--- service-worker.js
+++ service-worker.js
@@ -8,9 +8,9 @@ if ('serviceWorker' in navigator) {
 // Install service worker
 const CACHE_NAME = 'hello-world-cache';
 const urlsToCache = [
-  '/',
-  '/pages/cats.html',
-  '/pages/dogs.html'
+  '/hello-world-pwa/',
+  'hello-world-pwa//pages/cats.html',
+  'hello-world-pwa/pages/dogs.html'
 ];
 
 self.addEventListener('install', function(event) {
